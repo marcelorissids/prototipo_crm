23114a1d687ade474712c1dde6e5e0f400c8b2ec
Marcelo Rissi <marcelorissi2@gmail.com>
2023-09-11 16:47:47 -0300

    teste9

--- Home.py
+++ Home.py
@@ -90,13 +90,13 @@ if arquivo is not None:
     # botão para realizar a analise
     if st.button('Realizar Análise'):
         with st.spinner('Realizando análise...'):
-            df1 = analise_rfm(df)
+            df = analise_rfm(df)
 
         st.write('Resultado da análise:')
-        st.write(df1)
+        st.write(df)
 
         # botão para exportar 
-        csv = df1.to_csv(index=False, encoding='utf-8')
+        csv = df.to_csv(index=False, encoding='utf-8')
         b64 = base64.b64encode(csv.encode()).decode()
-        href = f'<a href="data:file/csv,base64,{b64}" download="df1.csv">Baixar Resultado CSV</a>'
+        href = f'<a href="data:file/csv,base64,{b64}" download="df.csv">Baixar Resultado CSV</a>'
         st.markdown(href, unsafe_allow_html=True)
